39eaf46e9396d62726cc1aa2d6df88adb283ba8d
riesposi <riccardo.esposito@lnf.infn.it>
2025-06-11 22:34:53 +0000

    modified .bob and .sub

--- iocBoot/icpdasiocSample/ioc-icpdas7026test.cmd
+++ iocBoot/icpdasiocSample/ioc-icpdas7026test.cmd
@@ -13,16 +13,15 @@ modbusInterposeConfig("ICPDAS_IP",0,0,0)
 drvModbusAsynConfigure ("ICP_get_port", "ICPDAS_IP", 1, 1, 0,  6, 0, 1000, "ICPDAS7026")
 drvModbusAsynConfigure ("ICP_set_port", "ICPDAS_IP", 1, 5, 0,  6, 0, 1000, "ICPDAS7026")
 
-drvModbusAsynConfigure("RANGE_OUT", "ICPDAS_IP", 1, 6, 459, 6, 0, 1000, "ICPDAS7026")
+drvModbusAsynConfigure("RANGE_OUT", "ICPDAS_IP", 1, 6, 459, 2, 0, 1000, "ICPDAS7026")
 drvModbusAsynConfigure("RANGE_IN", "ICPDAS_IP", 1, 6, 427, 6, 0, 1000, "ICPDAS7026")
+drvModbusAsynConfigure("DO_OUTPUT", "ICPDAS_IP", 1, 5, 0, 2, 0, 1000, "ICPDAS7026")
+drvModbusAsynConfigure("DI_OUTPUT", "ICPDAS_IP", 1, 2, 0, 2, 0, 1000, "ICPDAS7026")
+drvModbusAsynConfigure("PORTMIN", "ICPDAS_IP", 1, 3, 0, 6, 0, 1000, "ICPDAS7026")
 
 
 
-drvModbusAsynConfigure ("ICP_info_port", "ICPDAS_IP", 1, 4, 150, 4, 0, 1000, "ICPDAS")
-
-
-
-dbLoadRecords("$(TOP)/db/icp7026.db","P=ICPDAS, R=Test7026, get_port=ICP_get_port, set_port=ICP_set_port, PORTMIN=RANGE_IN, PORTMAX=RANGE_OUT, PORTINFO=ICP_info_port, PORT=ICP_info_port")
+dbLoadRecords("$(TOP)/db/icp7026.db","P=ICPDAS, R=Test7026, get_port=ICP_get_port, set_port=ICP_set_port, PORTRANGEIN=RANGE_IN, PORTRANGEOUT=RANGE_OUT, PORTINFO=ICP_info_port,DOPORT=DO_OUTPUT,CNTPORT=DI_OUTPUT, PORTMIN=PORTMIN")
 
 
 iocInit()
