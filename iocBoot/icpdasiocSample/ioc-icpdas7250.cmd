dd67cf7c62f52c86ea4066c8d4df3c0f8419e986
amichelo <andrea.michelotti@lnf.infn.it>
2025-09-07 16:38:11 +0200

    added DO_RB

--- iocBoot/icpdasiocSample/ioc-icpdas7250.cmd
+++ iocBoot/icpdasiocSample/ioc-icpdas7250.cmd
@@ -20,7 +20,7 @@ drvModbusAsynConfigure ("ICP_info_port", "ICPDAS_IP", 1, 4, 150, 4, 0, 1000, "IC
 
 #dbLoadRecords("$(TOP)/db/icp7250.db","P=ICPDAS,R=Test7250,DIPORT=ICP_DI,CNTPORT=ICP_DICNT,DOPORT=ICP_DO, PORTINFO=ICP_info_port")
 
-dbLoadRecords("$(TOP)/db/icpdi.template","P=ICPDAS,R=Test7267,PORT=ICP_DI,CNTPORT=ICP_DICNT, ID=0,IDX2=0")
+dbLoadRecords("$(TOP)/db/icpdi.template","P=ICPDAS,R=Test7267,PORT=ICP_DI,PORTCNT=ICP_DICNT, ID=0,IDX2=0")
 dbLoadRecords("$(TOP)/db/icpdo.template","P=ICPDAS,R=Test7267,PORT=ICP_DO, ID=0")
 dbLoadRecords("$(TOP)/db/icpdasVersion.template","P=ICPDAS,R=Test7267, PORT=ICP_info_port")
 
